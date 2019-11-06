898bfa9d9dabef1b939748e81344a4de225b87fc
Saroj Das <imit.saroj@gmail.com>
2019-11-06 16:53:39 +0530

    SpringBootWithDrools

---
+++ src/main/java/com/example/springdrools/SpringBootWithDrools/SpringBootWithDroolsApplication.java
@@ -0,0 +1,21 @@
+package com.example.springdrools.SpringBootWithDrools;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootWithDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDroolsApplication.class, args);
+	}
+	
+	@Bean
+	public KieContainer kieContainer() {
+		return KieServices.Factory.get().getKieClasspathContainer();
+	}
+
+}
---
+++ src/test/java/com/example/springdrools/SpringBootWithDrools/SpringBootWithDroolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springdrools.SpringBootWithDrools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWithDroolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
